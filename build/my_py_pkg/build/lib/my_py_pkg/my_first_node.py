bd74f58a86116a11cf114188af8052ba19053fe0
Mertcangndn <mertcangndn@gmail.com>
2026-01-29 12:06:54 +0300

    timercallback updated

--- build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
+++ build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
@@ -6,11 +6,14 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("python_test") #Node'un ismini buraya yazıyoruz.
+
+        self.counter = 0
         self.get_logger().info("Hello, I'm alive!")
         self.create_timer(0.5,self.timer_callback)
     
     def timer_callback(self):
-        self.get_logger().info("Hello")
+        self.counter += 1
+        self.get_logger().info(f"Hello {self.counter}")
 
 def main(args=None):
     rclpy.init(args=args)
